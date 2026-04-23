23a095f83648d3e81740cfeaa7407b5f733e770e
Shedrach Elutilo <shedrache20@gmail.com>
2026-04-22 22:56:48 -0500

    Add files via upload

---
+++ infra/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.5"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
